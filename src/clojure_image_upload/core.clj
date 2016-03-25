9c858a764be771f128ab876be026f044a1008b5b
ericweidman <ericweidman@gmail.com>
2016-03-25 13:48:54 -0400

    Added dependents

--- src/clojure_image_upload/core.clj
+++ src/clojure_image_upload/core.clj
@@ -1,4 +1,10 @@
 (ns clojure-image-upload.core
+    (:require [ring.adapter.jetty :as j]
+            [compojure.core :as c]
+            [hiccup.core :as h]
+            [ring.middleware.params :as p]
+            [ring.util.response :as r])
+  
   (:gen-class))
 
 (defn -main []
