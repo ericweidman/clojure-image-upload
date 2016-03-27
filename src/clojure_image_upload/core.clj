4685a5192ce586f6163eb3550e1013f9c79ee8d9
ericweidman <ericweidman@gmail.com>
2016-03-27 11:06:35 -0400

    Added upload button

--- src/clojure_image_upload/core.clj
+++ src/clojure_image_upload/core.clj
@@ -10,10 +10,34 @@
 (defonce pictures (atom[]))
 (defonce server (atom nil))
 
-(add-watch messages :save-to-disk
+(add-watch pictures :save-to-disk
   (fn [_ _ _ _]
     (spit "pictures.edn" (pr-str @pictures))))
 
+(c/defroutes app
+  (c/GET "/" request
+   (h/html [:html
+            [:body
+             [:form {:action "/add-picture" :enctype "multipart/form-data" :method "post"}
+              [:input {:type "file" :placeholder "Upload an image" :name "picture"}]
+              [:button {:type "Submit"} "Upload Photo"]]
+             [:ul
+              (map (fn [picture]
+                     [:li pictures])
+                @pictures)]]]))
+
+  (c/POST "/add-picture" request
+    (let [picture (get (:params request) "picture")]
+      (swap! pictures conj picture)
+      (r/redirect "/"))))
 
 (defn -main []
-  (println "Hello, world!"))
+  (try
+    (let [pictures-str (slurp "pictures.edn")
+          pictures-vec (read-string pictures-str)]
+      (reset! pictures pictures-vec))
+    (catch Exception _))
+  
+  (when @server
+    (.stop @server))
+  (reset! server (j/run-jetty (p/wrap-params app) {:port 8080 :join? false})))
